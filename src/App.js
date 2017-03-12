8483af8f7d9e4c5f5561f4d0c0d1c5158720482f
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2017-03-12 15:31:20 +0100

    Clean version

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import quotes from '../../../quotes.json';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">Hello PWr!
+      </div>
+    );
+  }
+}
+
+export default App;
