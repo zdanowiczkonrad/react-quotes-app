8f685a33d4363785a19b11b999c1874997b6a69d
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2017-03-12 16:46:56 +0100

    Binded like actions to state

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import { Quote } from './quote';
 
 class App extends Component {
 
-  onQuoteLiked = (quote) => {
+  constructor(props) {
+    super(props);
+    this.state = { quotes, likedQuotes: [] };
+  }
+
+  onQuoteLiked = quote => {
     console.log('quoteLiked', quote);
+    if (this.state.likedQuotes.indexOf(quote) < 0) {
+      const withQuote = this.state.likedQuotes.concat(quote);
+      this.setState({ likedQuotes: withQuote });
+    } else {
+      const withoutQuote = this.state.likedQuotes.filter(one => one !== quote);
+      this.setState({likedQuotes: withoutQuote});
+    }
   };
 
   render() {
     return (
       <div className="App">
         <h1>Quotes React App</h1>
-       {quotes.map(quote => <Quote
+       {this.state.quotes.map(quote => <Quote
         quote={quote}
         onQuoteLiked={this.onQuoteLiked}
+        likedQuotes={this.state.likedQuotes}
        />)}
       </div>
     );
--- src/quote.js
+++ src/quote.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import images from './images/images';
 import './quotes.css';
 
-export const Quote = ({quote, onQuoteLiked}) =>
+export const Quote = ({quote, onQuoteLiked, likedQuotes}) =>
   <div className="quote">
       <img className="avatar" src={images[quote.author]}/>
       <div className="content">
         <span className="author">{quote.author}
           <button
             onClick={() => onQuoteLiked(quote)}
-          className={`like` + (quote.author === 'Mr. Meeseeks' ? ' active' : '') }>Like ❤️️️️</button>
-          </span>
+            className={`like` + (likedQuotes.indexOf(quote) > -1 ? ' active' : '') }>
+              Like ❤️️️️
+          </button>
+        </span>
         <span className="text">{quote.text}</span>
     </div>
   </div>;
