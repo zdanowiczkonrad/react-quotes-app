fd0d04ea44bf040288e29be5cdf81146803716b7
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2017-03-12 16:51:38 +0100

    Tweaked like button

--- src/quote.js
+++ src/quote.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import images from './images/images';
 import './quotes.css';
 
-export const Quote = ({quote, onQuoteLiked, likedQuotes}) =>
-  <div className="quote">
+export const Quote = ({quote, onQuoteLiked, likedQuotes}) => {
+  const isLiked = likedQuotes.indexOf(quote) > -1;
+  return  <div className="quote">
       <img className="avatar" src={images[quote.author]}/>
       <div className="content">
         <span className="author">{quote.author}
           <button
             onClick={() => onQuoteLiked(quote)}
-            className={`like` + (likedQuotes.indexOf(quote) > -1 ? ' active' : '') }>
-              Like ❤️️️️
+            className={`like` + (isLiked ? ' active' : '') }>
+              {isLiked && '❤️️'} Like
           </button>
         </span>
         <span className="text">{quote.text}</span>
     </div>
   </div>;
+}
+
