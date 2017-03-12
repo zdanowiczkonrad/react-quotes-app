b648e125d8f17d3f6a31ca6c188a6a74f3d1b2b7
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2017-03-12 16:12:14 +0100

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import quotes from '../../../quotes.json';
+import './quotes.css';
 import images from './images/images';
 
-import './quotes.css';
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -15,7 +15,6 @@ class App extends Component {
               <span className="author">Rick</span>
               <span className="text">If I let you make me nervous, then we can't get schwifty</span>
           </div>
-          
         </div>
       </div>
     );
