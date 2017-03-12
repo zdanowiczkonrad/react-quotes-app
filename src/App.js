8861501f850317a643fe7b18ed6a59ae316ffa46
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2017-03-12 16:11:38 +0100

    Added characters meta and rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import quotes from '../../../quotes.json';
+import images from './images/images';
 
+import './quotes.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">Hello PWr!
+      <div className="App">
+        <h1>Quotes React App</h1>
+        <div className="quote">
+            <img className="avatar" src={images.Rick}/>
+            <div className="content">
+              <span className="author">Rick</span>
+              <span className="text">If I let you make me nervous, then we can't get schwifty</span>
+          </div>
+          
+        </div>
       </div>
     );
   }
---
+++ src/images/images.js
@@ -0,0 +1,9 @@
+import rick from './rick.png';
+import morty from './morty.png';
+import meeseeks from './mr. meeseeks.jpg';
+
+export default {
+    "Rick": rick,
+    "Morty": morty,
+    "Mr. Meeseeks": meeseeks
+}
