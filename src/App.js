30dd11d284f6917809b59816b4787c15ed20caaa
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2017-03-12 16:15:02 +0100

    Refactored Rick quote as a component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import images from './images/images';
 
 class App extends Component {
 
+
   render() {
+    const rickQuote = {
+      author: 'Rick',
+      text: 'If I let you make me nervous, then we can\'t get schwifty'
+    };
+
     return (
       <div className="App">
         <h1>Quotes React App</h1>
-        <div className="quote">
-            <img className="avatar" src={images.Rick}/>
-            <div className="content">
-              <span className="author">Rick</span>
-              <span className="text">If I let you make me nervous, then we can't get schwifty</span>
-          </div>
-        </div>
+        {renderQuote(rickQuote)}
       </div>
     );
   }
 }
+
+const renderQuote = quote => <div className="quote">
+            <img className="avatar" src={images[quote.author]}/>
+            <div className="content">
+              <span className="author">{quote.author}</span>
+              <span className="text">{quote.text}</span>
+          </div>
+        </div>;
 
 export default App;
